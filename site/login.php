03710f21db49bea4896d62df807d86464fc0f633
Clankcoll <Clankcoll@gmail.com>
2023-11-28 22:03:34 +0100

    Enhacnment
    Changed the Login PHP also added the Proxmox picture

--- site/login.php
+++ site/login.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>PS-UI Login</title>
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
 </head>
 <body>
     <?php
@@ -33,12 +34,37 @@
     }
     ?>
 
-    <form method="post" action="login.php">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username"><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br>
-        <input type="submit" value="Submit">  
-    </form>
+<section class=" text-center text-lg-start">
+  <div class="card mb-3">
+    <div class="row g-0 d-flex align-items-center">
+      <div class="col-lg-4 d-none d-lg-flex">
+        <img src="./style/pictures/proxmox-logo-stacked-color.svg" alt="Proxmox Logo"
+          class="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5" />
+      </div>
+      <div class="col-lg-8">
+        <div class="card-body py-5 px-md-5">
+
+          <form method="post" action="login.php">
+            <!-- Username Input -->
+            <div class="form-outline mb-4">
+              <input type="text" id="username" name="username" class="form-control" />
+              <label class="form-label" for="Username">Username</label>
+            </div>
+
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+              <input type="password" id="form2Example2" name="password" class="form-control" />
+              <label class="form-label" for="password">Password</label>
+            </div>
+            <!-- Submit button -->
+            <button type="button" class="btn btn-primary btn-block mb-4">Log In</button>
+
+          </form>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 </body>
 </html>
